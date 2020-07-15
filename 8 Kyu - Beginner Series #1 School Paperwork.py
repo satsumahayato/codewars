54a4de46f12da1ac2f54c65c342f7040bd462f91
satsumahayato <68173317+satsumahayato@users.noreply.github.com>
2020-07-14 21:39:34 -0400

    Add files via upload

---
+++ 8 Kyu - Beginner Series #1 School Paperwork.py
@@ -0,0 +1,9 @@
+# 8 Kyu
+# Beginner Series #1 School Paperwork
+# https://www.codewars.com/kata/55f9b48403f6b87a7c0000bd/train/python
+
+def paperwork(n, m):
+    if (n < 0) or (m < 0):
+        return 0
+    else:
+        return (n * m)
